b37df88305504093df14ac88bbab246211b737c3
mslovero <mslovero@brown.gob.ar>
2026-04-27 11:13:00 -0300

    prueb

--- geriatrico/config/database.php
+++ geriatrico/config/database.php
@@ -96,6 +96,10 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => [
+                PDO::ATTR_TIMEOUT => 5,
+                PDO::ATTR_PERSISTENT => false,
+            ],
         ],
 
         'sqlsrv' => [
